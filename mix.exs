af2a4d4ff7baa442a4452b73f52536e7ec35d4e3
Alan Vardy <alanvardy@gmail.com>
2019-06-26 18:28:28 -0600

    add credo

--- mix.exs
+++ mix.exs
@@ -42,7 +42,9 @@ defmodule Health.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      # tooling
+      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false}
     ]
   end
 
