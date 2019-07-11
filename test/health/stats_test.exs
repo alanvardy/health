10a0c28eee2d3fef757055272585b29255cd2a3a
Alan Vardy <alanvardy@gmail.com>
2019-07-10 21:12:22 -0600

    spacing

--- test/health/stats_test.exs
+++ test/health/stats_test.exs
@@ -20,13 +20,13 @@ defmodule Health.StatsTest do
 
       assert log_dates == trend_dates
     end
+
     test "returns empty list when passed an empty list" do
       assert Stats.build_trends([]) == []
     end
   end
 
   describe "log" do
-
     test "list_log/0 returns all logs" do
       user = insert(:user)
       log = insert(:log, user: user)
@@ -42,36 +42,35 @@ defmodule Health.StatsTest do
       assert left == right
     end
 
-      test "create_log/1 with valid data creates a log" do
-        user = insert(:user)
-        assert {:ok, %Log{} = log} = Stats.create_log(params_for(:log, user: user))
-        assert log.date == Timex.today
-        assert log.weight == 230
-      end
+    test "create_log/1 with valid data creates a log" do
+      user = insert(:user)
+      assert {:ok, %Log{} = log} = Stats.create_log(params_for(:log, user: user))
+      assert log.date == Timex.today()
+      assert log.weight == 230
+    end
 
-      test "create_log/1 with invalid data returns error changeset" do
-        assert {:error, %Ecto.Changeset{}} = Stats.create_log(params_for(:log, weight: 50))
-      end
+    test "create_log/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Stats.create_log(params_for(:log, weight: 50))
+    end
 
-      test "update_log/2 with valid data updates the log" do
-        log = insert(:log)
-        assert {:ok, %Log{} = log} = Stats.update_log(log, params_for(:log, weight: 400))
-        assert log.date == Timex.today()
-        assert log.weight == 400
-      end
+    test "update_log/2 with valid data updates the log" do
+      log = insert(:log)
+      assert {:ok, %Log{} = log} = Stats.update_log(log, params_for(:log, weight: 400))
+      assert log.date == Timex.today()
+      assert log.weight == 400
+    end
 
-      test "update_log/2 with invalid data returns error changeset" do
-        log = insert(:log)
-        assert {:error, %Ecto.Changeset{}} = Stats.update_log(log, params_for(:log, weight: 1000))
-        # assert log == Stats.get_log!(log.id)
-      end
+    test "update_log/2 with invalid data returns error changeset" do
+      log = insert(:log)
+      assert {:error, %Ecto.Changeset{}} = Stats.update_log(log, params_for(:log, weight: 1000))
+      # assert log == Stats.get_log!(log.id)
+    end
 
-      test "delete_log/1 deletes the log" do
-        log = insert(:log)
-        assert {:ok, %Log{}} = Stats.delete_log(log)
-        assert_raise Ecto.NoResultsError, fn -> Stats.get_log!(log.id) end
-      end
-
+    test "delete_log/1 deletes the log" do
+      log = insert(:log)
+      assert {:ok, %Log{}} = Stats.delete_log(log)
+      assert_raise Ecto.NoResultsError, fn -> Stats.get_log!(log.id) end
+    end
   end
 
   defp remove_user([log]), do: [%Log{log | user: nil}]
