ee0fe5887acba079948d066f46c01b93a9df6ede
Alan Vardy <alanvardy@gmail.com>
2019-07-15 19:28:50 -0600

    more refactoring

--- lib/health/weight/stats.ex
+++ lib/health/weight/stats.ex
@@ -1,4 +1,4 @@
-defmodule Health.Weight.Statistics do
+defmodule Health.Weight.Stats do
   @moduledoc "Weight log and associated calculations"
   defstruct logs: [], adjusted_weights: [], trend: []
 end
