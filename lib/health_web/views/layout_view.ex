ee2dd0db4f0ccc76f0b57b4cc5e13dfd73cc4ebc
Matt Riemer <matthew@codeandeffect.com>
2019-07-18 09:00:25 -0600

    wip

--- lib/health_web/views/layout_view.ex
+++ lib/health_web/views/layout_view.ex
@@ -36,4 +36,127 @@ defmodule HealthWeb.LayoutView do
     content_tag(:li, link(text, opts), class: nav_class)
   end
 
+  def tabs(opts \\ [], fun) when is_function(fun, 1) do
+    nav_opts = [class: "nav nav-tabs", role: "tablist"] ++ opts
+    div_opts = [class: "tab-content"]
+
+    tabs = %{tabmode: :nav}
+    nav_tag = content_tag(:ul, fun.(tabs), nav_opts)
+
+    tabs = %{tabmode: :div}
+    div_tag = content_tag(:div, fun.(tabs), div_opts)
+
+    [nav_tag, div_tag]
+  end
+
+  def tab(f, label, opts \\ [], [do: _] = block) do
+    id = label |> String.downcase |> String.replace(" ", "-")
+
+    nav_opts = [href: "#", class: "nav-link", id: "#{id}-tab", role: "tab", "data-toggle": "tab", "aria-controls": id, "aria-selected": false]
+    div_opts = [class: "tab-pane", id: id, role: "tabpanel", "aria-labelledby": "#{id}-tab"]
+
+    case f.tabmode do
+      :nav -> content_tag(:li, content_tag(:a, label, nav_opts), class: "nav-item")
+      :div -> content_tag(:div, div_opts, block)
+    end
+  end
+
+  # def tab(%{tabmode: :div}, label, opts \\ [], [do: _] = block) do
+  #   id = label |> String.downcase |> String.replace(" ", "-")
+
+  #   div_opts = [class: "tab-pane", id: id, role: "tabpanel", "aria-labelledby": "#{id}-tab"]
+
+  #   content_tag(:div, div_opts, block)
+  # end
+
+  #<div class="tab-pane active" id="home" role="tabpanel" aria-labelledby="home-tab">...</div>
+
+# <!-- Nav tabs -->
+# <ul class="nav nav-tabs" id="myTab" role="tablist">
+#   <li class="nav-item">
+#     <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
+#   </li>
+#   <li class="nav-item">
+#     <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
+#   </li>
+#   <li class="nav-item">
+#     <a class="nav-link" id="messages-tab" data-toggle="tab" href="#messages" role="tab" aria-controls="messages" aria-selected="false">Messages</a>
+#   </li>
+#   <li class="nav-item">
+#     <a class="nav-link" id="settings-tab" data-toggle="tab" href="#settings" role="tab" aria-controls="settings" aria-selected="false">Settings</a>
+#   </li>
+# </ul>
+
+  # def tabs(active: nil, unique: false, list: {}, content: {}, &block)
+  #   raise 'expected a block' unless block_given?
+
+  #   @_tab_mode = :tablist
+  #   @_tab_active = (active || :first)
+  #   @_tab_unique = ''.object_id if unique
+
+  #   content_tag(:ul, {class: 'nav nav-tabs', role: 'tablist'}.merge(list)) do
+  #     yield # Yield to tab the first time
+  #   end +
+  #   content_tag(:div, {class: 'tab-content'}.merge(content)) do
+  #     @_tab_mode = :content
+  #     @_tab_active = (active || :first)
+  #     yield # Yield to tab the second time
+  #   end
+  # end
+
+  # def tabs(opts \\ [], [do: _] = block) do
+  #   IO.puts("---- TABS -----")
+
+  #   nav_opts = [class: "nav nav-tabs", role: "tablist"] ++ opts
+  #   div_opts = [class: "tab-content"]
+
+  #   #block[:do] |> Tuple.append(:tabsmode)
+
+
+  #   block = block ++ {:tabsmode}
+
+  #   IO.inspect(block)
+
+  #   nav_tag = content_tag(:ul, nav_opts, block)
+  #   div_tag = content_tag(:div, div_opts, block)
+
+  #   IO.puts("---- RET -----")
+
+  #   [nav_tag, div_tag]
+  # end
+
+  # def tab(label, opts \\ [], [do: _] = block) do
+  #   id = label |> String.downcase |> String.replace(" ", "-")
+
+  #   IO.puts("---- TAB -----")
+
+  #   nav_opts = [href: "#", class: "nav-link", id: "#{id}-tab", role: "tab", "data-toggle": "tab", "aria-controls": id, "aria-selected": false]
+
+  #   IO.inspect(block)
+
+  #   content_tag(:li, class: "nav-item") do
+  #     content_tag(:a, label, nav_opts)
+  #   end
+
+  # end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
