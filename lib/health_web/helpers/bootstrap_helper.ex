5b1a1c5fcb85f81ad18a8b2c7fdf71d674e707b1
Matt Riemer <matthew@codeandeffect.com>
2019-07-24 11:55:25 -0600

    better line lengths

--- lib/health_web/helpers/bootstrap_helper.ex
+++ lib/health_web/helpers/bootstrap_helper.ex
@@ -72,23 +72,19 @@ defmodule HealthWeb.BootstrapHelper do
 
   # Bootstrap4 - Tab
   # Works with tabs helper
-  #
-  # <%= tabs(active: "Two") do %>
-  #   <%= tab("One") do %>
-  #     <p>This is tab one</p>
-  #   <% end %>
-
-  #   <%= tab("Two") do %>
-  #     <p>This is tab two</p>
-  #   <% end %>
-  # <% end %>
-  #
   def tab(label, opts \\ [], [do: _] = block) do
     id = label |> String.downcase |> String.replace(" ", "-")
     controls = Kernel.to_charlist(id) |> Enum.sum
 
-    nav_opts = [class: "nav-link", id: "tab-#{id}-#{controls}", role: "tab", href: "##{id}-#{controls}", "data-toggle": "tab", "aria-controls": "#{id}-#{controls}", "aria-selected": false]
-    div_opts = [class: "tab-pane fade", id: "#{id}-#{controls}", role: "tabpanel", "aria-labelledby": "tab-#{id}-#{controls}"]
+    nav_opts = [
+      class: "nav-link", id: "tab-#{id}-#{controls}", role: "tab", href: "##{id}-#{controls}",
+      "data-toggle": "tab", "aria-controls": "#{id}-#{controls}", "aria-selected": false
+    ]
+
+    div_opts = [
+      class: "tab-pane fade", id: "#{id}-#{controls}", role: "tabpanel",
+      "aria-labelledby": "tab-#{id}-#{controls}"
+    ]
 
     nav_tag = content_tag(:li, content_tag(:a, label, nav_opts), class: "nav-item")
     div_tag = content_tag(:div, div_opts, block)
@@ -96,16 +92,16 @@ defmodule HealthWeb.BootstrapHelper do
     [nav_tag, div_tag]
   end
 
-  defp tabs_get_navs(list), do: Enum.map(list, fn x -> x |> List.first end)
-  defp tabs_get_divs(list), do: Enum.map(list, fn x -> x |> List.last end)
+  defp tabs_get_navs(list), do: list |> Enum.map(&List.first/1)
+  defp tabs_get_divs(list), do: list |> Enum.map(&List.last/1)
 
   defp tabs_find_index(list, active) when active == :first, do: 0
   defp tabs_find_index(list, active) do
     list |> tabs_get_navs |> Enum.find_index(fn x -> x |> List.flatten |> Enum.member?(active) end)
   end
 
-  defp tabs_activate_navs(list, index), do: List.update_at(list, index, fn x -> tabs_activate_nav(x) end)
-  defp tabs_activate_divs(list, index), do: List.update_at(list, index, fn x -> tabs_activate_div(x) end)
+  defp tabs_activate_navs(list, index), do: list |> List.update_at(index, &tabs_activate_nav/1)
+  defp tabs_activate_divs(list, index), do: list |> List.update_at(index, &tabs_activate_div/1)
 
   defp tabs_activate_nav(tags) do
     tags |> raw |> safe_to_string |> String.replace("nav-link", "nav-link active", global: false)
