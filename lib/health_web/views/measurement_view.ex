f38ddadf4fc89196acca1c83dccbc8a4d41a5d30
Alan Vardy <alanvardy@gmail.com>
2019-07-28 20:57:56 -0600

    improve measurements

--- lib/health_web/views/measurement_view.ex
+++ lib/health_web/views/measurement_view.ex
@@ -1,3 +1,11 @@
 defmodule HealthWeb.MeasurementView do
   use HealthWeb, :view
+
+  @spec average([number]) :: float
+  def average(list) do
+    list
+    |> Enum.reduce(0, fn x, acc -> x + acc end)
+    |> Kernel./(length(list))
+    |> Float.round(1)
+  end
 end
