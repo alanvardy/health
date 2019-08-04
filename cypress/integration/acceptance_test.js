a51cbafb9aca6de43de3691e39f8ce7d6d4f0a2d
Alan Vardy <alanvardy@gmail.com>
2019-08-04 08:57:22 -0600

    more acceptance tests

--- cypress/integration/acceptance_test.js
+++ cypress/integration/acceptance_test.js
@@ -1,4 +1,9 @@
 describe('Acceptance Test', function () {
+  const password = 'securepassword'
+  const name = 'Dufus McDumbface'
+  const new_name = 'Smarty McSmartface'
+  const email = 'testemail@test.com'
+
   beforeEach(function () {
     // before each test, we can automatically preserve the
     // 'session_id' and 'remember_token' cookies. this means they
@@ -12,22 +17,70 @@ describe('Acceptance Test', function () {
 
   it('Visits the main page', function () {
     cy.visit('http://localhost:5000')
-
   })
+
+  it('Uses the contact form', function () {
+    cy.contains('Contact').click()
+    cy.get('input#message_name').type(name)
+    cy.get('input#message_from_email').type(email)
+    cy.get('textarea#message_message').type('What a great website Alan!')
+    cy.get('[type=submit]').click()
+    cy.contains('Your message has been sent!')
+  })
+
   it('Registers an account', function () {
     cy.contains('Register').click()
-    cy.get('input#user_email').type('test@test.com')
-    cy.get('input#user_name').type('Dufus McDumbface')
-    cy.get('input#user_password').type('securepassword')
-    cy.get('input#user_confirm_password').type('securepassword')
+    cy.get('input#user_email').type(email)
+    cy.get('input#user_name').type(name)
+    cy.get('input#user_password').type(password)
+    cy.get('input#user_confirm_password').type(password)
     cy.get('[type=submit]').click()
     cy.contains('Welcome back Dufus McDumbface')
   })
+
   it('Enters a weight record', function () {
     cy.contains('Weight').click()
     cy.get('#trend').find('input#log_weight').type('234')
     cy.get('#trend').find('[type=submit]').click()
     cy.contains('Recent Logs').click()
     cy.contains('234.0')
+  })
+
+  it('Enters measurements', function () {
+    cy.contains('Measurements').click()
+    cy.contains('New').click()
+    cy.get('input#measurement_left_bicep').type('234')
+    cy.get('input#measurement_right_bicep').type('234')
+    cy.get('input#measurement_left_thigh').type('234')
+    cy.get('input#measurement_right_thigh').type('234')
+    cy.get('input#measurement_chest').type('234')
+    cy.get('input#measurement_waist').type('234')
+    cy.get('input#measurement_buttocks').type('99')
+    cy.get('[type=submit]').click()
+    cy.contains('99.0')
+  })
+
+  it('Changes name', function () {
+    cy.contains(name).click()
+    cy.contains('Settings').click()
+    cy.get('input#user_name').clear()
+    cy.get('input#user_name').type(new_name)
+    cy.get('input#user_current_password').type(password)
+    cy.get('[type=submit]').click()
+    cy.contains(new_name)
+    cy.contains('Your account has been updated.')
+  })
+
+  it('Signs out', function () {
+    cy.contains(new_name).click()
+    cy.contains('Sign Out').click()
+    cy.contains('Goodbye!')
+  })
+
+  it('Logs back in', function () {
+    cy.get('input#user_email').type(email)
+    cy.get('input#user_password').type(password)
+    cy.get('[type=submit]').click()
+    cy.contains('Welcome back')
   })
 })
